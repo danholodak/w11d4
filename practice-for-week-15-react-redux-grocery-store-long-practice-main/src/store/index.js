ba7ecd185500cbf0309194ede0ddda34d450300b
Gary <geehor@gmail.com>
2023-03-23 14:45:04 -0400

    Finish Phase 1

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/index.js
@@ -1,6 +1,21 @@
 // define root reducer and a function that will return a redux store
-import { createStore, combineReducers, applyMiddleware, compose } from 'react';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 const rootReducer = combineReducers({
 
 });
+
+let enhancer;
+
+if (process.env.NODE_ENV !== "production") {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
