705089d63e14582a4d273a62796ce8b5cdec732c
Dan Holodak <danholodak@gmail.com>
2023-03-23 14:31:10 -0400

    first commit

---
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/index.js
@@ -0,0 +1,6 @@
+// define root reducer and a function that will return a redux store
+import { createStore, combineReducers, applyMiddleware, compose } from 'react';
+
+const rootReducer = combineReducers({
+
+});
